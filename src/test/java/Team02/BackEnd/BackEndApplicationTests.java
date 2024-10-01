9bc0c9b978a6d1cd4a3aeb357f88c98f9802d9ea
Hyun0828 <spdlqj4818@naver.com>
2024-10-01 23:44:44 +0900

    Initial Setting

---
+++ src/test/java/Team02/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Team02.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
