e903172f2169c8e341f1727742a31815514ead5a
Hyun0828 <spdlqj4818@naver.com>
2024-10-25 16:18:59 +0900

    fix: api에 /api/spring prefix 추가

--- src/main/java/Team02/BackEnd/controller/ReissueController.java
+++ src/main/java/Team02/BackEnd/controller/ReissueController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/spring")
 public class ReissueController {
 
     private final JwtService jwtService;
