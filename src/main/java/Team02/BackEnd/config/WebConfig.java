9f6bf2d830b0156fc5638f38bd6b2c5463c1c141
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-10-26 14:42:43 +0900

    fix: swagger 배포용 cors 매핑

--- src/main/java/Team02/BackEnd/config/WebConfig.java
+++ src/main/java/Team02/BackEnd/config/WebConfig.java
@@ -4,6 +4,7 @@ import Team02.BackEnd.oauth.OauthServerTypeConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new OauthServerTypeConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/v3/api-docs/**").allowedOrigins("https://peachmentor.com");
+        registry.addMapping("/swagger-ui/**").allowedOrigins("https://peachmentor.com");
     }
 }
