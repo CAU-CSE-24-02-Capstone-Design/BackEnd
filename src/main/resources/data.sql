a7fcc96e4bb1b61b7bb22ef853facaa7d5080f3c
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-10-30 23:56:50 +0900

    fix(data.sql): 테스트용 유저 더미 생성
    test@example.com

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 
 Insert INTO question(question_index, description) values(1, '당신은 어떤 도형을 닮은 것 같나요? 그 도형을 닮은 이유는 무엇인가요? 1분동안 말해보세요!');
+INSERT INTO user(email, name, oauth_server_id, voice_url, oauth_server, role) VALUES ('test@example.com','test', '1', 'test voice url', 'KAKAO', 'USER');
 
