6ec4a79c5fda7418b49268a8f3a91d2a6d66a2bc
Hyun <71479783+Hyun0828@users.noreply.github.com>
2024-11-04 16:04:54 +0900

    refactor: 코드 리팩토링 (#12)

--- src/main/java/Team02/BackEnd/controller/QuestionController.java
+++ src/main/java/Team02/BackEnd/controller/QuestionController.java
@@ -1,5 +1,8 @@
 package Team02.BackEnd.controller;
 
+import static Team02.BackEnd.constant.Constants.ACCESS_TOKEN_PREFIX;
+import static Team02.BackEnd.constant.Constants.ACCESS_TOKEN_REPLACEMENT;
+
 import Team02.BackEnd.apiPayload.ApiResponse;
 import Team02.BackEnd.apiPayload.code.status.SuccessStatus;
 import Team02.BackEnd.converter.QuestionConverter;
@@ -25,8 +28,8 @@ public class QuestionController {
     @GetMapping("/question")
     @Operation(summary = "질문 요청", description = "유저가 클릭시 공개 될 질문 가져오기")
     public ApiResponse<QuestionResponseDto.GetQuestionDto> getQuestion(@RequestHeader("Authorization") String authorizationHeader) {
-        String accessToken = authorizationHeader.replace("Bearer ", "");
-        Question question = questionService.getQuestionDescription(accessToken);
+        String accessToken = authorizationHeader.replace(ACCESS_TOKEN_PREFIX, ACCESS_TOKEN_REPLACEMENT);
+        Question question = questionService.getUserQuestion(accessToken);
         Long answerId = answerService.getAnswerId(accessToken, question);
 
         return ApiResponse.of(SuccessStatus.GET_QUESTION, QuestionConverter.toQuestionResponseDto(question, answerId));
--- src/main/java/Team02/BackEnd/repository/UserRepository.java
+++ src/main/java/Team02/BackEnd/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     Optional<User> findByOauthId(OauthId oauthId);
 
