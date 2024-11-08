b30d13c69f3cc043f08340cb176a97273e19e8c0
Hyun <71479783+Hyun0828@users.noreply.github.com>
2024-11-09 00:42:30 +0900

    [fix] feedback 엔티티 업데이트 내역 저장 (#16)
    * [fix] feedback 엔티티 업데이트 내역 저장
    
    * fix(getBeforeAudioLink): feedback 생성 시 answer는 userId가 아닌 answerId로 찾아와서 연결한다
    
    * debug(getBeforeAudioLink): answerId 디버깅
    
    * debug(getBeforeAudioLink): answerId 디버깅
    
    * fix(Feedback): User와 다대일로 변경
    
    * fix(FeedbackService): user와 연결된 answer는 여러개

--- src/main/java/Team02/BackEnd/service/AnswerService.java
+++ src/main/java/Team02/BackEnd/service/AnswerService.java
@@ -6,11 +6,14 @@ import Team02.BackEnd.domain.Question;
 import Team02.BackEnd.domain.oauth.User;
 import Team02.BackEnd.exception.validator.AnswerValidator;
 import Team02.BackEnd.repository.AnswerRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AnswerService {
 
     private final UserService userService;
@@ -25,10 +28,10 @@ public class AnswerService {
         return answer.getId();
     }
 
-    public Answer getAnswerByUserId(Long userId) {
-        Answer answer = answerRepository.findByUserId(userId);
-        AnswerValidator.validateAnswerIsNotNull(answer);
-        return answer;
+    public List<Answer> getAnswersByUserId(Long userId) {
+        List<Answer> answers = answerRepository.findByUserId(userId);
+        answers.forEach(AnswerValidator::validateAnswerIsNotNull);
+        return answers;
     }
 
     public Answer getAnswerByAnswerId(Long answerId) {
