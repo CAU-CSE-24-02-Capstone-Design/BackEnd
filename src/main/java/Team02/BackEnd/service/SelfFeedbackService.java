b6c56bb01a540d8a617b62d15a9696bada316dca
Hyun0828 <spdlqj4818@naver.com>
2024-11-14 00:38:42 +0900

    fix(FeedbackController): 피드백 생성 요청과 피드백 데이터 요청을 구분

--- src/main/java/Team02/BackEnd/service/SelfFeedbackService.java
+++ src/main/java/Team02/BackEnd/service/SelfFeedbackService.java
@@ -3,6 +3,7 @@ package Team02.BackEnd.service;
 import Team02.BackEnd.converter.SelfFeedbackConverter;
 import Team02.BackEnd.domain.Answer;
 import Team02.BackEnd.domain.SelfFeedback;
+import Team02.BackEnd.domain.oauth.User;
 import Team02.BackEnd.dto.SelfFeedbackRequestDto.SaveSelfFeedbackDto;
 import Team02.BackEnd.exception.validator.SelfFeedbackValidator;
 import Team02.BackEnd.repository.SelfFeedbackRepository;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class SelfFeedbackService {
 
     private final SelfFeedbackRepository selfFeedbackRepository;
+    private final UserService userService;
     private final AnswerService answerService;
 
     public void saveSelfFeedback(Long answerId, SaveSelfFeedbackDto saveSelfFeedbackDto) {
@@ -22,15 +24,15 @@ public class SelfFeedbackService {
         selfFeedbackRepository.save(selfFeedback);
     }
 
-    public SelfFeedback getBeforeSelfFeedbackByAnswer(Long answerId) {
-        Answer answer = answerService.getAnswerByAnswerId(answerId);
+    public SelfFeedback getBeforeSelfFeedback(String accessToken) {
+        User user = userService.getUserByToken(accessToken);
+        Answer answer = answerService.getLatestAnswerByUserId(user.getId());
         return getSelfFeedbackByAnswerId(answer.getId());
     }
 
     public SelfFeedback getSelfFeedbackByAnswerId(Long answerId) {
         SelfFeedback selfFeedback = selfFeedbackRepository.findByAnswerId(answerId);
         SelfFeedbackValidator.validateSelfFeedbackIsNotNull(selfFeedback);
-
         return selfFeedback;
     }
 }
