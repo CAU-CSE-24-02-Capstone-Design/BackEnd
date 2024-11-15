a0ab171625d96d615e573c363a09041796b17e72
Hyun0828 <spdlqj4818@naver.com>
2024-11-15 16:03:34 +0900

    fix(InsightController): api url에 prefix 추가

--- src/main/java/Team02/BackEnd/controller/InsightController.java
+++ src/main/java/Team02/BackEnd/controller/InsightController.java
@@ -12,11 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/spring")
 @Slf4j
 public class InsightController {
 
