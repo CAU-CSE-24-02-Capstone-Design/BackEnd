916a42aec30915892a033fe34da3d5451b228ff5
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-11-15 18:32:40 +0900

    chores(data.sql): 오타

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ values (4, '당신이 가장 좋아하는 계절은 무엇인가요? 그 계절�
 Insert INTO question(question_index, description)
 values (5, '자신의 인생을 영화로 만든다면, 어떤 장면이 가장 중요하다고 생각하나요? 그 장면을 선택한 이유를 1분 동안 말해보세요!');
 Insert INTO question(question_index, description)
-values (6, '당신은 어떤 도형을 닮은 것 같나요? 그 도형을 닮은 이유는 무엇인가요? 1분동안 말해보세요!);
+values (6, '당신은 어떤 도형을 닮은 것 같나요? 그 도형을 닮은 이유는 무엇인가요? 1분동안 말해보세요!');
 
 
 INSERT INTO user(created_at, email, name, oauth_server_id, voice_url, oauth_server, role)
