868ee7373853daea5875fc4f1302ff638be79ebf
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-11-15 18:30:01 +0900

    fix(data.sql): 질문 순서 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 Insert INTO question(question_index, description)
-values (1, '당신은 어떤 도형을 닮은 것 같나요? 그 도형을 닮은 이유는 무엇인가요? 1분동안 말해보세요!');
+values (1, '자신을 한 단어로 표현한다면, 어떤 단어가 가장 적합하다고 생각하나요? 그 단어를 고른 이유를 1분 동안 말해보세요!');
 Insert INTO question(question_index, description)
 values (2, '당신은 어떤 색을 닮았다고 생각하나요? 그 색을 닮은 이유는 무엇인가요? 1분 동안 설명해보세요!');
 Insert INTO question(question_index, description)
@@ -9,7 +9,7 @@ values (4, '당신이 가장 좋아하는 계절은 무엇인가요? 그 계절�
 Insert INTO question(question_index, description)
 values (5, '자신의 인생을 영화로 만든다면, 어떤 장면이 가장 중요하다고 생각하나요? 그 장면을 선택한 이유를 1분 동안 말해보세요!');
 Insert INTO question(question_index, description)
-values (6, '자신을 한 단어로 표현한다면, 어떤 단어가 가장 적합하다고 생각하나요? 그 단어를 닮은 이유를 1분 동안 말해보세요!');
+values (6, '당신은 어떤 도형을 닮은 것 같나요? 그 도형을 닮은 이유는 무엇인가요? 1분동안 말해보세요!);
 
 
 INSERT INTO user(created_at, email, name, oauth_server_id, voice_url, oauth_server, role)
