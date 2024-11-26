47f425b8219ef6cd4d19e04418af8e3d9edcebd9
Hyun0828 <spdlqj4818@naver.com>
2024-11-26 23:03:17 +0900

    feat(Test): controller test 추가

--- src/test/java/Team02/BackEnd/controller/CalendarControllerTest.java
+++ src/test/java/Team02/BackEnd/controller/CalendarControllerTest.java
@@ -1,13 +1,14 @@
 package Team02.BackEnd.controller;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import Team02.BackEnd.jwt.service.JwtService;
+import Team02.BackEnd.service.calendar.CalendarCheckService;
 import Team02.BackEnd.service.user.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.stream.Stream;
@@ -20,8 +21,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(UserController.class)
-class UserControllerTest {
+@WebMvcTest(CalendarController.class)
+public class CalendarControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,27 +31,40 @@ class UserControllerTest {
     private ObjectMapper objectMapper;
 
     @MockBean
-    private UserService userService;
-
-    @MockBean
     private JwtService jwtService;
 
-    @DisplayName("회원 탈퇴")
+    @MockBean
+    private CalendarCheckService calendarCheckService;
+
+    @DisplayName("사용자의 특정 년, 월에 대한 스피치 기록 가져오기")
     @Test
     @WithMockUser(value = "tlsgusdn4818@gmail.com", roles = {"USER"})
-    void signOut() throws Exception {
+    void getDatesWhenUserDid() throws Exception {
         // given
         String accessToken = "mockAccessToken";
         given(jwtService.createAccessToken("tlsgusdn4818@gmail.com")).willReturn(accessToken);
 
+        String year = "2024";
+        String month = "11";
+
         // when
+        Long[] answerIdDidThisPeriod = Stream.generate(() -> 0L).
+                limit(32).
+                toArray(Long[]::new);
+        given(calendarCheckService.getDatesWhenUserDid(accessToken, year, month)).willReturn(answerIdDidThisPeriod);
+
         // then
-        mockMvc.perform(delete("/api/spring/sign-out")
+        mockMvc.perform(get("/api/spring/calendars")
                         .with(csrf())
                         .header("Authorization", "Bearer " + accessToken)
+                        .param("year", year)
+                        .param("month", month)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.isSuccess").value(true))
-                .andExpect(jsonPath("$.code").value("USER2000"))
-                .andExpect(jsonPath("$.message").value("회원 탈퇴 성공"));
+                .andExpect(jsonPath("$.code").value("CALENDAR2000"))
+                .andExpect(jsonPath("$.message").value("유저가 참여한 날짜 가져오기 성공"))
+                .andExpect(jsonPath("$.result", hasSize(32)))
+                .andExpect(jsonPath("$.result[0]", equalTo(0)))
+                .andExpect(jsonPath("$.result[31]", equalTo(0)));
     }
 }
--- src/test/java/Team02/BackEnd/controller/FeedbackControllerTest.java
+++ src/test/java/Team02/BackEnd/controller/FeedbackControllerTest.java
@@ -5,9 +5,11 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import Team02.BackEnd.domain.Feedback;
 import Team02.BackEnd.jwt.service.JwtService;
 import Team02.BackEnd.service.feedback.FeedbackCheckService;
 import Team02.BackEnd.service.feedback.FeedbackService;
@@ -62,48 +64,75 @@ class FeedbackControllerTest {
                 .andExpect(jsonPath("$.message").value("피드백 저장 성공"));
     }
 
-//    @DisplayName("피드백 데이터 요청 api")
-//    @Test
-//    @WithMockUser(value = "tlsgusdn4818@gmail.com", roles = {"USER"})
-//    void getFeedback() throws Exception {
-//        // given
-//        Long answerId = 1L;
-//        Feedback mockFeedback = createMockFeedback();
-//
-//        // when
-//        given(feedbackService.getFeedbackByAnswerId(answerId))
-//                .willReturn(mockFeedback);
-//
-//        // then
-//        String expectedJson = """
-//                {
-//                  "isSuccess": true,
-//                  "code": "FEEDBACK2001",
-//                  "message": "피드백 가져오기 성공",
-//                  "result": {
-//                    "beforeScript": "bs",
-//                    "beforeAudioLink": "ba",
-//                    "afterScript": "as",
-//                    "afterAudioLink": "aa",
-//                    "feedbackText": "ft"
-//                  }
-//                }
-//                """;
-//        mockMvc.perform(get("/api/spring/feedbacks")
-//                        .param("answerId", String.valueOf(answerId))
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(content().json(expectedJson))
-//                .andDo(print());
-//    }
-//
-//    private Feedback createMockFeedback() {
-//        return Feedback.builder()
-//                .beforeAudioLink("ba")
-//                .beforeScript("bs")
-//                .afterAudioLink("aa")
-//                .afterScript("as")
-//                .feedbackText("ft")
-//                .build();
-//    }
+    @DisplayName("피드백 데이터 요청 api")
+    @Test
+    @WithMockUser(value = "tlsgusdn4818@gmail.com", roles = {"USER"})
+    void getFeedback() throws Exception {
+        // given
+        String accessToken = "mockAccessToken";
+        given(jwtService.createAccessToken("tlsgusdn4818@gmail.com")).willReturn(accessToken);
+
+        Long answerId = 1L;
+        Feedback mockFeedback = createMockFeedback();
+
+        // when
+        given(feedbackCheckService.getFeedbackByAnswerId(answerId))
+                .willReturn(mockFeedback);
+
+        // then
+        String expectedJson = """
+                {
+                  "isSuccess": true,
+                  "code": "FEEDBACK2001",
+                  "message": "피드백 가져오기 성공",
+                  "result": {
+                    "beforeScript": "bs",
+                    "beforeAudioLink": "ba",
+                    "afterScript": "as",
+                    "afterAudioLink": "aa",
+                    "feedbackText": "ft"
+                  }
+                }
+                """;
+        mockMvc.perform(get("/api/spring/feedbacks")
+                        .with(csrf())
+                        .header("Authorization", "Bearer " + accessToken)
+                        .param("answerId", String.valueOf(answerId))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson))
+                .andDo(print());
+    }
+
+    @DisplayName("오늘 답변 했는지 여부 받아오기")
+    @Test
+    @WithMockUser(value = "tlsgusdn4818@gmail.com", roles = {"USER"})
+    void doSpeechToday() throws Exception {
+        // given
+        String accessToken = "mockAccessToken";
+        given(jwtService.createAccessToken("tlsgusdn4818@gmail.com")).willReturn(accessToken);
+
+        // when
+        given(feedbackCheckService.doSpeechToday(accessToken)).willReturn(true);
+
+        // then
+        mockMvc.perform(get("/api/spring/feedbacks/completions")
+                        .with(csrf())
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.code").value("ANSWER2000"))
+                .andExpect(jsonPath("$.message").value("오늘 답변 했는 지 여부 가져오기 성공"));
+    }
+
+    private Feedback createMockFeedback() {
+        return Feedback.builder()
+                .beforeAudioLink("ba")
+                .beforeScript("bs")
+                .afterAudioLink("aa")
+                .afterScript("as")
+                .feedbackText("ft")
+                .build();
+    }
 }
